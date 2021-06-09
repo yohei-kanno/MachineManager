133c4e307ea85b5eb571ba6fb1101af7d3b8a4a7
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-06-09 11:37:46 +0900

    Fix: config environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,5 +3,6 @@ const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.get('vue').options.extractCSS = false
 environment.loaders.prepend('vue', vue)
 module.exports = environment
