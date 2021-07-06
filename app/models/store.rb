cd63bf3425908b4d0dff8e66557b5f2e56d60d8d
yohei-kanno <74027006+yohei-kanno@users.noreply.github.com>
2021-07-06 19:50:29 +0900

    Fix: ストアモデルのバリデーション追加

--- app/models/store.rb
+++ app/models/store.rb
@@ -7,13 +7,22 @@ class Store < ApplicationRecord
   has_many :places, dependent: :destroy
   
   validates :name, presence: true
-  validates :pachinko_num, presence: true, numericality: true, length:{maximum: 4}
-  validates :slot_num, presence: true, numericality: true, length:{maximum: 4}
+  validates :pachinko_num, presence: true, numericality: true, length:{
+    maximum: 4,
+    message: "は4桁で入力して下さい"
+  }
+  
+  validates :slot_num, presence: true, numericality: true, length:{
+    maximum: 4,
+    message: "は4桁で入力して下さい"
+  }
+  
   validates :code, presence: true, numericality: true, uniqueness: true, 
   length:{
     maximum: 8,
     minimum: 8,
     message: "は8桁数字で入力して下さい" 
   }
+  
   validates :agreement, acceptance: { allow_nil: false, message: "に同意して下さい", on: :create }
 end
